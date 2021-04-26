407a1bad36b67e2e274b9e91e41870604ce8b478
Vítor Emídio Simoncini <vitor.simoncini@ufv.br>
2021-04-26 20:43:00 -0300

    Modificando a main, porém ainda sem importância

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include <vector>
 #include "Cartas.h"
+#include "Baralho.h"
 
 int main(){
-	std::vector<Cartas*> carta;
-	for(int i=1; i<=13; i++) for(int j=1; j<=4; j++) carta.push_back(new Cartas(i, j));
-	for(int i=0; i<52; i++){carta[i]->imprimeCarta();std::cout << "\n\n";}
+	Baralho baralho;
+	for(int j=0; j<100; j++){
+		for(int i=0; i<4*4; i++){
+			baralho.embaralhaCartas();
+			baralho.imprimeCarta(i);
+		}
+		std::cout << "\n";
+	}
 	return 0;
 }
