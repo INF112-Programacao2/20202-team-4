4438b071024fcd68a03ed22b8c3f2550a8df923a
Vítor Emídio Simoncini <vitor.simoncini@ufv.br>
2021-05-03 09:47:49 -0300

    Começando a escrever o código de Corajoso

---
+++ Corajoso.h
@@ -0,0 +1,12 @@
+#ifndef CORAJOSO_H
+#define CORAJOSO_H
+
+#include "Entidades.h"
+
+class Corajoso:public Entidades{
+public:
+	Corajoso(std::string nome, int pontos);
+	int calculaJogada(bool turnoInicial, std::vector<Cartas> cartasJogadas) override;
+};
+
+#endif
