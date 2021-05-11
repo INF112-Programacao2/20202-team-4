fbbb10a9327ea0c7aa571d32034adf2692f608a7
jcssilveira <62386472+jcssilveira@users.noreply.github.com>
2021-05-11 15:51:13 -0300

    Header da Classe atualizado

---
+++ Calculista.h
@@ -0,0 +1,13 @@
+#ifndef CALCULISTA_H
+#define CALCULISTA_H
+
+#include "Entidades.h"
+
+class Calculista:public Entidades
+{
+public:
+	Calculista(std::string nome, int pontos);
+	int calculaJogada(bool turnoInicial, std::vector<Cartas> cartasJogadas) override;
+};
+
+#endif
