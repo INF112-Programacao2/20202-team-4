fb333f1739c272271c321813928c72482977f5c6
Vítor Emídio Simoncini <vitor.simoncini@ufv.br>
2021-05-11 13:05:12 -0300

    Erros consertados e agora Corajoso sabe jogar cartas :D

--- Corajoso.cpp
+++ Corajoso.cpp
@@ -1,6 +1,5 @@
-#include <cstdlib>
 #include <ctime>
-#include <iostream>
+#include <cstdlib>
 #include "Corajoso.h"
 
 Corajoso::Corajoso(std::string nome, int pontos):Entidades(nome, pontos){}
@@ -11,16 +10,34 @@ int Corajoso::calculaJogada(bool turnoInicial, std::vector<Cartas> cartasJogadas
 	if(turnoInicial){
 		srand(time(NULL));
 		
-		int quantidadeTurnosApostados = 0;
+		this->turnosApostados = 0;
 		
 		for(int i=0; i<cartasDaMaoOrdenadas.size(); i++){
-			if(cartasDaMaoOrdenadas[i].get_ehCartaEspecial() && cartasDaMaoOrdenadas[i].get_numero() != 4){
-				if(rand()%100 > 40) quantidadeTurnosApostados++;
+			if(cartasDaMaoOrdenadas[i].get_ehCartaEspecial()){
+				if(cartasDaMaoOrdenadas[i].get_numero() != 4){
+					if(rand()%100 > 40) this->turnosApostados++;
+				}else{
+					this->turnosApostados++;
+				}
 			}
 			
-			else if(cartasDaMaoOrdenadas[i].get_numero() == 3 || cartasDaMaoOrdenadas[i].get_numero() == 2) quantidadeTurnosApostados++;
+			else if(cartasDaMaoOrdenadas[i].get_numero() == 3 || cartasDaMaoOrdenadas[i].get_numero() == 2) this->turnosApostados++;
 		}
 		
-		return quantidadeTurnosApostados;
+		return this->turnosApostados;
+	}else{
+		cartasJogadas = ordenaCartas(cartasJogadas);
+		int quantidadeCartas = cartasDaMaoOrdenadas.size();
+		
+		if(cartasJogadas.size() == 0) return quantidadeCartas >= 1 ? quantidadeCartas-1 : 0;
+		
+		if(this->turnosApostados == 0){
+			for(int i=0; i<quantidadeCartas; i++)
+				if(comparaForcaCartas(cartasDaMaoOrdenadas[i], cartasJogadas[0])) return i;
+		}else{
+			if(!comparaForcaCartas(cartasDaMaoOrdenadas[0], cartasJogadas[0])) return 0;
+		}
+		
+		return quantidadeCartas >= 1 ? quantidadeCartas-1 : 0;
 	}
 }
