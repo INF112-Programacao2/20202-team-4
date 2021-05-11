af043a345773acec938f4612e70cb16438195122
Vítor Emídio Simoncini <vitor.simoncini@ufv.br>
2021-05-11 13:05:34 -0300

    Nada demais por aqui

--- main.cpp
+++ main.cpp
@@ -3,15 +3,24 @@
 #include "Cartas.h"
 #include "Baralho.h"
 #include "Entidades.h"
+#include "Corajoso.h"
 
 int main(){
-	Baralho baralho;
-	for(int j=0; j<100; j++){
-		for(int i=0; i<4*4; i++){
-			baralho.embaralhaCartas();
-			baralho.imprimeCarta(i);
-		}
+	Corajoso c("corajosin", 0);
+	std::vector<Cartas> cartasJogadas;
+	cartasJogadas.push_back(Cartas(7, 1));
+	c.adicionaCarta(Cartas(1, 1));
+	c.adicionaCarta(Cartas(11, 4));
+	c.adicionaCarta(Cartas(7, 2));
+	c.adicionaCarta(Cartas(4, 1));
+	c.adicionaCarta(Cartas(3, 1));
+		
+	c.ordenaCartasDaMao();
+	for(int i=0; i<5; i++){
+		c.imprimeCarta(i);
 		std::cout << "\n";
 	}
+	std::cout << "\n";
+	c.imprimeCarta(c.calculaJogada(0, cartasJogadas));
 	return 0;
 }
