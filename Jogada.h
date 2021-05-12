062261b335788145e64b72d4caaadd42460cbc9c
jcssilveira <62386472+jcssilveira@users.noreply.github.com>
2021-05-12 16:09:14 -0300

    Algumas modificações

--- Jogada.h
+++ Jogada.h
@@ -3,25 +3,29 @@
 
 #include <vector>
 #include "Baralho.h"
+#include "Entidades.h"
 
 class Jogada{
 private:
     int rodada;
     int turno;
     bool turnoInicial;
-    int jogadorInicial;
+    int jogadorInicialRodada;
+    int jogadorInicialTurno;
     Baralho baralho;
     std::vector<Entidades*> jogadores;
+    std::vector<Cartas> cartasJogadas;
 
 public:
-    Jogada(); //sem parametros momentaneamente 
+    Jogada(); //sem parametros momentaneamente
+    ~Jogada();
     int get_rodada();
-    int get_turno();
-    void adicionaJogador();
-    void distribuirCartas();
-    void mudaTurno();
-    void mudaJogadorInicial();
-    void jogaTurno();
+    int get_turno(); 
+    void distribuirCartas(); //turno == numero de cartas pra cada jogador   
+    void determinaVencedorTurno(); //cartasJogadas
+    void jogaTurno(); //std::vector<Entidades*> jogadores são chamados pra fazer as jogadas, armazenadas em cartasJogadas, parametro de determinaVencedorTurno()
+    void mudaTurno(); //turno++, if(turno==rodada) turno = 0 bool turnoInicial == true
+    void mudaJogadorInicial(); //if(turnoInicial = true) jogadorInicialRodada++, else jogadorInicialTurno == ganhador anterior
 };
 
 #endif
