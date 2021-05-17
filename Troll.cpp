46eb119a23b55b73b0fdead36f034743fc969e76
Vítor Emídio Simoncini <vitor.simoncini@ufv.br>
2021-05-17 16:41:43 -0300

    Troll implementado

---
+++ Troll.cpp
@@ -0,0 +1,14 @@
+#include <ctime>
+#include <cstdlib>
+#include "Troll.h"
+
+Troll::Troll(std::string nome, int pontos):Entidades(nome, pontos){}
+
+
+int Troll::calculaJogada(bool turnoInicial, std::vector<Cartas> cartasJogadas){
+	srand(time(NULL));
+
+	if(turnoInicial){
+		return rand()%this->cartas.size(); //faz uma aposta aleatoria 
+	}else return 0; //joga uma carta aleatoria da mão
+}
---
+++ Troll.h
@@ -0,0 +1,12 @@
+#ifndef TROLL_H
+#define TROLL_H
+
+#include "Entidades.h"
+
+class Troll:public Entidades{
+public:
+	Troll(std::string nome, int pontos);
+	int calculaJogada(bool turnoInicial, std::vector<Cartas> cartasJogadas) override; 
+};
+
+#endif
