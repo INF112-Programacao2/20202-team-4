b675547e095ff696e839dc70a61f73ced8b17155
VitorESimoncini <62184095+VitorESimoncini@users.noreply.github.com>
2021-05-17 18:39:25 -0300

    Corrigindo indentação dnv

--- Humano.cpp
+++ Humano.cpp
@@ -4,27 +4,27 @@
 Humano::Humano(std::string nome, int pontos):Entidades(nome, pontos){}
 
 int calculaJogada(bool turnoInicial, std::vector<Cartas> cartasJogadas){
-  	std::vector<Cartas> cartasDaMaoOrdenadas = ordenaCartas(this->cartas);
-  
+	std::vector<Cartas> cartasDaMaoOrdenadas = ordenaCartas(this->cartas);
+
 	if(turnoInicial){ //Se for o inicio da rodada, as apostas serão feitas
 		std::cout << "Sua aposta: ";
 		do{
 			cin >> this->turnosApostados;
-        }while(this->turnosApostados < 0 || this->turnosApostados >= this->cartas.size()); //tratamento de exceção para que o usuario nao escolha uma aposta fora do escopo 
+		}while(this->turnosApostados < 0 || this->turnosApostados >= this->cartas.size()); //tratamento de exceção para que o usuario nao escolha uma aposta fora do escopo 
+
+		return this->turnosApostados;
+	}else{
+	std::cout << "\n\nSuas cartas sao: "
+	for (int i = 0; i < this->cartas.size(); i++){
+	    std::cout << i+1 << ": ";
+		cartas[i].imprimeCarta(); 
+		std::cout << (i == this->cartas.size()-1 ? "\n" : ", "); //imprimir virgula e a proxima carta ate o final, imprimindo um \n
+	}
       
-      return this->turnosApostados;
-    }else{
-		std::cout << "\n\nSuas cartas sao: "
-      	for (int i = 0; i < this->cartas.size(); i++){
-            std::cout << i+1 << ": ";
-          	cartas[i].imprimeCarta(); 
-          	std::cout << (i == this->cartas.size()-1 ? "\n" : ", "); //imprimir virgula e a proxima carta ate o final, imprimindo um \n
-        }
-      
-      	int posicaoEscolhida;
-      	std::cout << "\n\nEscolha uma carta (pela posicao): ";
-      	do{
-          std::cin >> posicaoEscolhida;
-        }while(posicaoEscolhida < 0 || posicaoEscolhida >= this->cartas.size()); //tratamento de exceção para que o usuario nao escolha uma carta fora do escopo 
+	int posicaoEscolhida;
+	std::cout << "\n\nEscolha uma carta (pela posicao): ";
+	do{
+		std::cin >> posicaoEscolhida;
+	}while(posicaoEscolhida < 0 || posicaoEscolhida >= this->cartas.size()); //tratamento de exceção para que o usuario nao escolha uma carta fora do escopo 
     }
 }
